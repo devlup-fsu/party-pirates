d48f17aefba0de846d3696742b8a5c0b7d83155a
joph <joogasl@gmail.com>
2023-10-24 21:51:03 -0400

    Added animation upon collection. Added optional property [variance] to add randomness to score. Fixed small issue where treasure would disappear after colliding with ANY entity.

--- Entities/Treasure/treasure.gd
+++ Entities/Treasure/treasure.gd
@@ -1,17 +1,24 @@
-extends Node2D
+extends Area2D
 
 @export var score_value := 5
+## If variance is not 0, player will gain a score_value +- variance.
+@export var variance := 0
 
+@onready var anim_player : AnimationPlayer = $AnimationPlayer
 
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 func _process(delta):
-    pass
+	pass
 
 
 func _on_body_entered(body):
-    if body.has_method("score_up"):
-        body.score_up(score_value)
-    queue_free() # TODO: Disappear animation with AnimationPlayer
+	if body.has_method("score_up"):
+		body.score_up(score_value + randi_range(-variance, variance))
+		anim_player.play("collected")
+		# Otherwise player may collide multiple times while the animation finishes.
+		$CollisionShape2D.disabled = true
+		await anim_player.animation_finished
+		queue_free()
