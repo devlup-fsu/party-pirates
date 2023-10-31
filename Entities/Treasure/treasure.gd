9dd877f172bad01e96735f7f047dbdf4eb2c4c67
joph <joogasl@gmail.com>
2023-10-31 19:47:45 -0400

    Added Whalen's assets into the project. Changed file organization appropriately. Changed Player's Sprite2D into AnimatedSprite2D.

--- Entities/Treasure/treasure.gd
+++ Entities/Treasure/treasure.gd
@@ -10,16 +10,16 @@ extends Area2D
 
 
 func _ready() -> void:
-    var animations = anim_sprite.sprite_frames.get_animation_names()
-    anim_sprite.animation = animations[randi() % animations.size()]
+	var animations = anim_sprite.sprite_frames.get_animation_names()
+	anim_sprite.animation = animations[randi() % animations.size()]
 
 
 func _on_body_entered(player: Player) -> void:
-    player.score_up(score_value + randi_range(-variance, variance))
-    
-    # Otherwise player may collide with this node multiple times while the animation finishes.
-    collision_shape.disabled = true
-    
-    anim_player.play("collected")
-    await anim_player.animation_finished
-    queue_free()
+	player.score_up(score_value + randi_range(-variance, variance))
+	
+	# Otherwise player may collide with this node multiple times while the animation finishes.
+	collision_shape.disabled = true
+	
+	anim_player.play("collected")
+	await anim_player.animation_finished
+	queue_free()
