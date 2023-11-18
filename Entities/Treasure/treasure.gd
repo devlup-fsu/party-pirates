773b22d7dcbb2082e6fda418c8888c0c57423598
Dion Tryban <github@diontryban.com>
2023-11-18 15:49:48 -0500

    Cleaned up code

--- Entities/Treasure/treasure.gd
+++ Entities/Treasure/treasure.gd
@@ -12,16 +12,3 @@ var is_collected := false
 func _ready() -> void:
 	var animations = anim_sprite.sprite_frames.get_animation_names()
 	anim_sprite.animation = animations[randi() % animations.size()]
-
-
-# TODO: Uncomment all of this!
-func _on_body_entered(player: Player) -> void:
-	pass
-	#player.score_up(score_value + randi_range(-variance, variance))
-	
-	# Otherwise player may collide with this node multiple times while the animation finishes.
-	#collision_shape.disabled = true
-	
-	#anim_player.play("collected")
-	#await anim_player.animation_finished
-	#queue_free()
