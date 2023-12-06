9165f387bdadb29c337376e1d84f93d2112eb064
Dion Tryban <github@diontryban.com>
2023-12-05 21:48:27 -0500

    Add shoot delay, cannon ball collision, and cannon ball despawning

--- Entities/CannonBall/cannon_ball.gd
+++ Entities/CannonBall/cannon_ball.gd
@@ -1,23 +1,36 @@
 class_name CannonBall
 extends Area2D
-const cannon_ball_scene: PackedScene = preload("res://Entities/CannonBall/cannon_ball.tscn")
+
+const CANNON_BALL_SCENE: PackedScene = preload("res://Entities/CannonBall/cannon_ball.tscn")
 const SPEED := 1000.0
+const ALIVE_TIME := 5.0
 
 var direction : Vector2 = Vector2.ZERO
 
 
 static func create(parent: Node, position: Vector2, direction: Vector2):
-	var cannon_ball: CannonBall = cannon_ball_scene.instantiate() 
+	var cannon_ball: CannonBall = CANNON_BALL_SCENE.instantiate() 
 	parent.add_child(cannon_ball)
 	cannon_ball.global_position = position
 	cannon_ball.direction = direction
+
+
+func _ready():
+	var despawnTimer = get_tree().create_timer(ALIVE_TIME)
+	despawnTimer.timeout.connect(_on_despawn_timer_timeout)
 
 
 func _physics_process(delta: float):
 	position+=(direction*SPEED*delta)
 
 
-func _on_body_entered(body: PhysicsBody2D):
+func _on_body_entered(body: Node2D):
 	if body is Player:
-		body.got_hit()
+		body.get_hit()
+	
+	if body is TileMap or body is Player:
 		queue_free()
+
+
+func _on_despawn_timer_timeout():
+	queue_free()
--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -8,6 +8,7 @@ const SPEED_DEGRADATION := 0.9
 
 @export var player := 0
 @export var cannon_parent : Node
+@export var shoot_delay := 0.5
 
 @onready var treasure_collector: TreasureCollector = $TreasureCollector
 @onready var vulnerability_timer: Timer = $VulnerabilityTimer
@@ -15,10 +16,10 @@ const SPEED_DEGRADATION := 0.9
 @onready var left_cannon_timer: Timer = $LeftCannon/Timer
 @onready var right_cannon: Marker2D = $RightCannon
 @onready var right_cannon_timer: Timer = $RightCannon/Timer
-@onready var cannon_scene: PackedScene = load("res://Entities/Cannons/cannon.tscn")
 
 var speed: float = 0
 var current_speed = MAX_SPEED 
+
 
 func _ready() -> void:
 	$AnimatedSprite2D.play("player" + str(self.player))
@@ -46,19 +47,16 @@ func _physics_process(delta: float) -> void:
 
 func _process(_delta: float):
 	var input: InputManager.InputProxy = InputManager.get_gamepad(player)
-# Temporary code to test on hit
-	if Input.is_action_just_pressed("test"):
-		got_hit()
-# Cannons, probably make a function later
+	
 	if input.is_shoot_left_pressed():
 		if left_cannon_timer.is_stopped():
 			CannonBall.create(cannon_parent, left_cannon.global_position, Vector2.UP.rotated(rotation))
-			left_cannon_timer.start()
+			left_cannon_timer.start(shoot_delay)
+	
 	if input.is_shoot_right_pressed():
 		if right_cannon_timer.is_stopped():
 			CannonBall.create(cannon_parent, right_cannon.global_position, Vector2.DOWN.rotated(rotation))
-			right_cannon_timer.start()
-		
+			right_cannon_timer.start(shoot_delay)
 
 
 func store_treasure() -> void:
