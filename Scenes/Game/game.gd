b4276826bbff0694a797e6e5e27bcfac93d722cc
Jordan <jfreyman11037@gmail.com>
2024-02-03 20:30:45 -0500

    Fixed game startup

--- Scenes/Game/game.gd
+++ Scenes/Game/game.gd
@@ -9,7 +9,7 @@ var PlayerScene = preload( "res://Entities/Player/player.tscn" )
 # Start the timer at 3 minutes, or 60 * 3 seconds.
 var time := 60 * 3
 
-func start():
+func _ready():
 	Scores.reset()
 	TIMER.start()
 	InputManager.game_started = true
--- Scenes/StartMenu/start_menu.gd
+++ Scenes/StartMenu/start_menu.gd
@@ -1,14 +1,8 @@
 extends Control
 
-@onready var GAME_SCENE: PackedScene = preload("res://Scenes/Game/game.tscn")
-
-
 func _enter_tree() -> void:
 	InputManager.game_started = false
 
 
 func _on_start_button_pressed() -> void:
-	var game = GAME_SCENE.instantiate()
-	get_tree().root.add_child(game)
-	game.start()
-	queue_free()
+	get_tree().change_scene_to_file("res://Scenes/Game/game.tscn")
