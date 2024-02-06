3183c42c8e43ec012e4485143b4cd5d93fac8a14
Jordan <jfreyman11037@gmail.com>
2024-02-05 21:29:24 -0500

    Add sorted character sprites to end screen

--- Scenes/EndScreen/end_screen.gd
+++ Scenes/EndScreen/end_screen.gd
@@ -1,14 +1,21 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func sort_descending(a, b):
+	if a[1] > b[1]:
+		return true
+	return false
 
+func _ready():
+	var scores = []
+	for i in range(4):
+		scores.append([i, Scores.get_player_score(i)])
+	scores.sort_custom(sort_descending)
+	var sprites = [$FirstPlace, $SecondPlace, $ThirdPlace, $FourthPlace]
+	for i in range(scores.size()):
+		sprites[i].play("player%d" % scores[i][0])
 
 func _on_play_again_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Game/game.tscn")
 	
-
-
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://Scenes/StartMenu/start_menu.tscn")
--- Scenes/Game/game.gd
+++ Scenes/Game/game.gd
@@ -7,7 +7,7 @@ extends Node2D
 var PlayerScene = preload( "res://Entities/Player/player.tscn" )
 
 # Start the timer at 3 minutes, or 60 * 3 seconds.
-var time := 3
+var time := 30
 
 func _ready():
 	Scores.reset()
