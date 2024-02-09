9fc29fb06765b2104517a6c662bd52011632859f
Jordan <jfreyman11037@gmail.com>
2024-02-08 19:10:53 -0500

    Added scores to end screen

--- Scenes/EndScreen/end_screen.gd
+++ Scenes/EndScreen/end_screen.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var score0: Label = $Score0
+@onready var score1: Label = $Score1
+@onready var score2: Label = $Score2
+@onready var score3: Label = $Score3
+
 func sort_descending(a, b):
 	if a[1] > b[1]:
 		return true
@@ -13,6 +18,11 @@ func _ready():
 	var sprites = [$FirstPlace, $SecondPlace, $ThirdPlace, $FourthPlace]
 	for i in range(scores.size()):
 		sprites[i].play("player%d" % scores[i][0])
+	
+	score0.text = str(scores[0][1])
+	score1.text = str(scores[1][1])
+	score2.text = str(scores[2][1])
+	score3.text = str(scores[3][1])
 
 func _on_play_again_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Game/game.tscn")
