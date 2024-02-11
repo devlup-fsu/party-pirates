a57510fd95ec26d04dc547ba7a67d2a839bb06c0
joph <joogasl@gmail.com>
2024-02-10 19:24:17 -0500

    Created Pickup2D

---
+++ Entities/Pickup/pickup2d.gd
@@ -0,0 +1,24 @@
+class_name Pickup2D
+extends Area2D
+
+@export var pickup: Pickup
+
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+
+var is_collected := false
+
+func _ready() -> void:
+	assert(pickup != null, "Pickup2D: Pickup resource must be set on the inspector.")
+	
+	var animations = anim_sprite.sprite_frames.get_animation_names()
+	anim_sprite.animation = animations[randi() % animations.size()]
+
+
+func collect(target: Player) -> void:
+	pickup.set_target(target)
+
+
+func drop(target: Player) -> void:
+	pass
--- Entities/Powerup/pickup.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@export var effect: Pickup
-
-
-func _player_contacted(player: Player):
-	print("Pickup - Player contacted")
-	player.accept_powerup(effect)
-	queue_free()
