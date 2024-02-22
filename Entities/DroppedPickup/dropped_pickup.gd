7c32057e881a2acb60cd206bb35969745cbc6b67
alectrem <tremblayalec14@gmail.com>
2024-02-22 00:27:37 -0500

    style compliance

--- Entities/DroppedPickup/dropped_pickup.gd
+++ Entities/DroppedPickup/dropped_pickup.gd
@@ -5,9 +5,11 @@ signal picked_up(dropped_pickup: DroppedPickup)
 
 @export var pickup: Pickup
 
+# range is for a suitably large random offset to bobbing per object
+@onready var _time: float = RandomNumberGenerator.new().randf_range(-1000.0, 10000.0)
+
 static var _random: RandomNumberGenerator = RandomNumberGenerator.new()
 
-@onready var _time: float = RandomNumberGenerator.new().randf_range(-1000.0, 1000.0)
 
 func _ready() -> void:
 	assert(pickup != null, "pickup must be set in DroppedPickup")
@@ -19,6 +21,7 @@ func pick_up(player: Player) -> void:
 	picked_up.emit(self)
 	pickup.pick_up(player)
 	queue_free()
+
 
 func _process(delta):
 	_time += delta
