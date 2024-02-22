da401f5ff43aa8f98292c141b462d48e3caf2637
alectrem <tremblayalec14@gmail.com>
2024-02-22 00:24:43 -0500

    adds bobbing pickups

--- Entities/DroppedPickup/dropped_pickup.gd
+++ Entities/DroppedPickup/dropped_pickup.gd
@@ -5,6 +5,9 @@ signal picked_up(dropped_pickup: DroppedPickup)
 
 @export var pickup: Pickup
 
+static var _random: RandomNumberGenerator = RandomNumberGenerator.new()
+
+@onready var _time: float = RandomNumberGenerator.new().randf_range(-1000.0, 1000.0)
 
 func _ready() -> void:
 	assert(pickup != null, "pickup must be set in DroppedPickup")
@@ -16,3 +19,8 @@ func pick_up(player: Player) -> void:
 	picked_up.emit(self)
 	pickup.pick_up(player)
 	queue_free()
+
+func _process(delta):
+	_time += delta
+	$Sprite2D.rotation = sin(_time) * 0.5
+	$Sprite2D.position.y = cos(_time) * 10
