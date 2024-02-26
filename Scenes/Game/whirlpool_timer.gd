f0728d831392a9e9f77a54ae10cff09989a9e589
Brownbookcover <mike.ridgeway123@gmail.com>
2024-02-26 01:41:44 -0500

    Started support for changing the location of whirlpools

--- Scenes/Game/whirlpool_timer.gd
+++ Scenes/Game/whirlpool_timer.gd
@@ -1,6 +1,8 @@
 extends Timer
 
 @export var water_node: Node
+@export var whirlpool_x: int
+@export var whirlpool_y: int
 
 var _whirlpool_scene = load("res://Entities/Whirlpool/whirlpool.tscn")
 
@@ -14,6 +16,10 @@ func _on_timeout():
 	var whirlpool = _whirlpool_scene.instantiate()
 	whirlpool.done.connect(done)
 	whirlpool.whirlpool_radius = whirlpool_size
+	whirlpool_x = 100
+	whirlpool_y = 100
+	#Needs to change the shaders location too
+	#whirlpool.position =  Vector2(whirlpool_x, whirlpool_y)
 	add_child(whirlpool)
 	water_node.material.set_shader_parameter("Whirlpool_Scale", whirlpool_size)
 	start(randi_range(30, 60))
